527016347a730ab0076927f29d3cd1207aa43217
JJ <jerry7722@hotmail.com>
2018-04-27 11:18:49 +0800

    add package vue-bootstrap

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,17 @@
 </template>
 
 <script>
-  export default {
-    name: 'App'
+import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+import Footer from '@/components/Footer'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+Vue.use(BootstrapVue);
+
+export default {
+  name: 'App',
   }
+}
 </script>
 
 <style lang="scss">
