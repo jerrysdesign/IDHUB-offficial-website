d3121ce62f718634961be07b276c666fd4972cab
JJ <jerry7722@hotmail.com>
2018-05-11 10:36:17 +0800

    update activities style

--- src/containers/Activities.vue
+++ src/containers/Activities.vue
@@ -1,19 +1,16 @@
 <template lang="pug">
 div(v-bind:class='pageName')
   Headbar
-  .team
+  .news
     .container
       .section-team
-        ul.team__box
-          li.team__item-wrapper(v-for='content in contents')
-            .ratio-1-1
-              .team__item
-                .item_title {{content.title}}
-                .item_img
-                  img(:src='content.image.url')
-                .item_date
-                  | {{content.publishedDate}}
-                .item_short_content(v-html="content.content.brief")
+        ul.news__box
+          li.news__item(v-for='content in contents')
+            .item_img
+              img(:src='content.image.url')
+            .item_date
+              | {{content.publishedDate}}
+            .item_short_content(v-html="content.content.brief")
 </template>
 
 <script>
@@ -63,87 +60,82 @@ export default {
 .section-team + .section-team {
   padding-top: 0;
 }
-.team {
-  &__item-wrapper {
+.news {
+  &__box {
+    margin: 0;
+    padding:  0;
+    @include clearfix;
     @include clear_list;
-    .ratio-1-1 {
-      width: 100%;
-      padding-top: 100%;
+  }
+  &__item {
+    background-color: white;
+    border-radius: 5px;
+    transition: box-shadow 0.4s ease;
+    padding: 15px;
+    box-shadow: 20px 20px 60px 0 rgba($color-gray-darkest,.1);
+    &:hover {
+      box-shadow: 20px 20px 60px 0 rgba($color-gray-darkest,.3);
+      .item_img {
+        background-image: linear-gradient(-225deg, #FA2B56 0%, #F91C3D 100%);
+      }
+    }
+    .item_img {
       position: relative;
+      background-color: #000;
+      overflow-x: hidden;
+      display:flex;
+      align-items:center;
+      justify-content:center;
+      height: 300px;
+      border-radius: 5px;
+      margin-bottom: 30px;
+      img {
+        height: 300px;
+        @media (#{$min_laptop}){
+        }
+      }
+    }
+    .item_date {
+      text-align: left;
+      font-size: 12px;
+      color: $color-gray-dark;
+    }
+    .item_short_content {
+      text-align: left;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      font-size: 18px;
+      line-height: 27px;
+      height: 4.5em;
+    }
+    .item_date + .item_short_content {
+      margin-top: 4px;
     }
   }
+}
+.news__title-box {
+  padding-bottom: 100px;
+}
+// grid
+.news {
+  background-color: $color-gray-ligtest;
+  &__item-wrapper {
+  }
+}
+.news {
   &__box {
     @include clearfix;
     margin: 0;
     padding:  0;
   }
   &__item {
-    position:  absolute;
-    top: 0;
-    left: 0;
-    bottom: 0;
-    right: 0;
-    background-color:$color-gray-light;
-    border-radius: 5px;
-    transition: box-shadow 0.4s ease;
-    padding-top: 20px;
-    padding-left: 10px;
-    padding-right: 10px;
-    padding-bottom: 50px;
-    &:hover {
-      // background-image: linear-gradient(-180deg, #1B47AD 0%, #0D2F80 100%);
-      // box-shadow: 30px 30px 60px 0 rgba($color-gray-darkest, .3);
-    }
-    @media (#{$max_phone}) {
-      padding: 20px 15px 20px;
-    }
-    .item_title {
-      font-size: 12px;
-    }
-    .item_img {
-      img {
-        width: 90%;
-      }
-    }
-    .item_date {
-      font-size: 11px;
-      color: #4f72cb;
-      text-align: left;
-      position: relative;
-      left: 1.5em;
-    }
-    .item_short_content {
-      // display:block;
-      text-align: center;
-      font-size: 12px;
-      overflow: hidden;
-      text-overflow: ellipsis;
-    }
-  }
-}
-.team__title-box {
-  padding-bottom: 100px;
-}
-// grid
-.team {
-  background-color: white;
-  &__item-wrapper {
-    @include gallery(6);
+    @include span(12 of 12);
     margin-bottom: gutter();
-    &:nth-last-child(-n+2) {
-      margin-bottom: 0;
+    @include tablet {
+      @include span(2 of 4);
     }
-    @media (#{$min_tablet}) {
-      @include gallery(4);
-      &:nth-last-child(-n+3) {
-        margin-bottom: 0;
-      }
-    }
-    @media (#{$min_desktop}) {
-      @include gallery(3);
-      &:nth-last-child(-n+4) {
-        margin-bottom: 0;
-      }
+    @include laptop  {
+      @include span(4 of 12);
     }
   }
 }
