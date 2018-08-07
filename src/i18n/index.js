4c41d012c21a812784c3002cfccc8a170ab98fca
JJ <jerry7722@hotmail.com>
2018-08-07 13:56:02 +0800

    update

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -3,7 +3,9 @@ import VueI18n from 'vue-i18n'
 import messages from './lang'
 Vue.use(VueI18n)
 
+const locale = localStorage.getItem('locale') || 'cn'
+
 export default new VueI18n({
-  locale: 'cn',
+  locale,
   messages
 })
