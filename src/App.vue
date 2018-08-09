63eea95c891cf18254b6947fb9e11a3ecf35b29a
JJ <jerry7722@hotmail.com>
2018-08-09 16:10:47 +0800

    update
    remove bootstrap-vue
    add bootstrap

--- src/App.vue
+++ src/App.vue
@@ -9,11 +9,9 @@
 import Vue from 'vue'
 import Sitemenu from '@/components/Menu'
 import Footbar from '@/components/Footer'
-import BootstrapVue from 'bootstrap-vue'
 import VueI18n from 'vue-i18n'
 import i18n from '@/i18n'
 import { store } from '@/store/header'
-Vue.use(BootstrapVue)
 Vue.use(VueI18n)
 Vue.config.productionTip = false
 
@@ -28,7 +26,5 @@ export default {
 }
 </script>
 <style lang="scss">
-  @import 'bootstrap-vue/dist/bootstrap-vue.css';
-  @import 'bootstrap/dist/css/bootstrap.css';
   @import './stylesheets/main';
 </style>
